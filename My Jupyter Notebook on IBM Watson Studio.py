f4ca8068c22a9c77c410c55d4006b790d0bd466f
elise20220615 <107527183+elise20220615@users.noreply.github.com>
2022-12-09 15:41:37 +0800

    Assignment

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# My Jupyter Notebook on IBM Watson Studio
+
+# **Li Yan**
+# 
+# Business Analyst
+
+# *I am interested in data science because I believe data science may change the world*
+
+# ### print(1+1)
+
+# In[1]:
+
+
+print(1+1)
+
+
+# > blockquote
+# 
+# --- 
+# ~strikethrough~
+
+# In[ ]:
+
+
+
+
